49c186f7046e631609e44d1768cbb56be279d7e2
HLl <HLl@com>
2021-08-22 18:20:03 +0800

    fourteen:@SentinelResources注解中fallback和blockHandler属性说明[38.39.40] Sentinel牛逼！

---
+++ 40_cloudalibaba_consumer_nacos_order84/src/main/java/com/whh/springcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,21 @@
+package com.whh.springcloud.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApplicationContextConfig {
+
+
+    /**
+     * Ribbon做服务调用和负载均衡
+     */
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+}
